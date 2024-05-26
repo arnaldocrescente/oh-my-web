5be7617d59e95120876e735935fabd15821c7fd5
arnaldo <arnaldo.crescente@kineton.it>
2024-05-26 21:52:01 +0200

    Fixed, icon problem with SVG in ios browser

--- libs/webfolio/main/src/lib/language-switcher/language-switcher.tsx
+++ libs/webfolio/main/src/lib/language-switcher/language-switcher.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import Icon from '@ant-design/icons';
+
 import { ItaFlag, UkFlag } from '@omw/shared/flags';
 
 import styles from './language-switcher.module.scss';
 
 export type SupportedLanguage = 'it' | 'en';
+
+interface Language {
+  title: string;
+  abbr: string;
+}
 
 export interface LanguageSwitcherProps {
   currentLanguage: SupportedLanguage;
@@ -15,10 +22,25 @@ export const LanguageSwitcher: React.FC<LanguageSwitcherProps> = React.memo(
   (props) => {
     const { onChange, currentLanguage } = props;
 
+    const languages: Record<SupportedLanguage, Language> = {
+      it: {
+        title: 'Italiano',
+        abbr: 'IT',
+      },
+      en: {
+        title: 'English',
+        abbr: 'EN',
+      },
+    };
+
     return (
-      <div className={styles['flag']} onClick={onChange}>
-        {currentLanguage === 'it' ? <UkFlag /> : <ItaFlag />}
-      </div>
+      <Icon
+        className={styles['flag']}
+        onClick={onChange}
+        component={currentLanguage === 'it' ? ItaFlag : UkFlag}
+        alt={languages[currentLanguage].abbr}
+        title={languages[currentLanguage].title}
+      />
     );
   }
 );
